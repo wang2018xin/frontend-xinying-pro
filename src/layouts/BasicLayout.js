24be3cf980f2ca5faacfacb9e3c6de261981c742
wang2018xin <wang2018xin@163.com>
2017-11-14 09:23:17 +0800

    basic layout

--- src/layouts/BasicLayout.js
+++ src/layouts/BasicLayout.js
@@ -1,17 +1,59 @@
 import React from 'react';
-
-
+import { Layout, Menu, Icon } from 'antd';
 import styles from './BasicLayout.less';
 
+const {Header, Sider, Content} = Layout;
 
-class BasicLayout extends React.PureComponent {
+export default class BasicLayout extends React.PureComponent {
+  state = {
+    collapsed: false,
+  };
+  toggle = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  }
 
   render() {
     return (
-      <div>BasicLayout...</div>
+      <Layout>
+        <Sider
+          trigger={null}
+          collapsible
+          collapsed={this.state.collapsed}
+        >
+          <div className={styles.logo}/>
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+            <Menu.Item key="1">
+              <Icon type="user"/>
+              <span>nav 1</span>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <Icon type="video-camera"/>
+              <span>nav 2</span>
+            </Menu.Item>
+            <Menu.Item key="3">
+              <Icon type="upload"/>
+              <span>nav 3</span>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Header style={{ background: '#fff', padding: 0 }}>
+            <Icon
+              className={styles.trigger}
+              type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+              onClick={this.toggle}
+            />
+          </Header>
+          <Content style={{ margin: '24px 16px', padding: 24, background: '#fff', minHeight: 280 }}>
+            Content
+          </Content>
+        </Layout>
+      </Layout>
     );
   }
 }
 
-export default BasicLayout;
+
 
